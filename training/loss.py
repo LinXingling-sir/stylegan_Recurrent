eb5c0d259663158be4d19ce293f11fc06664b379
LIN_SIR <139146193+LinXingling-sir@users.noreply.github.com>
2025-03-18 11:38:31 +0800

    Add files via upload

--- training/loss.py
+++ training/loss.py
@@ -1,3 +1,4 @@
+import numpy as np
 from torch.nn import functional as F
 import torch
 
@@ -10,6 +11,7 @@ class StyleGAN2Loss:
         self.G = G
         self.D = D
         self.r1_gamma = r1_gamma
+        self.bce = torch.nn.BCEWithLogitsLoss()
 
     def run_G(self, z, c):
         fake = self.G(z, c)
@@ -19,8 +21,9 @@ class StyleGAN2Loss:
         output = self.D(img, c)
         return output
 
-    def calc_D_loss(self, inputs, real_output, fake_output):
+    def calc_D_loss(self, inputs, real_output, fake_output, real_label, fake_label):
         d_loss_real = F.softplus(-real_output).mean()
+        loss_real = self.bce(real_output, real_label)
         gradients = torch.autograd.grad(
             outputs=real_output.sum(),  # 对标量输出求和计算梯度
             inputs=inputs,
@@ -30,18 +33,18 @@ class StyleGAN2Loss:
         r1_penalty = gradients.square().sum(dim=(1, 2, 3)).mean()
         r1_loss = self.r1_gamma * r1_penalty
         d_loss_fake = F.softplus(fake_output).mean()
-        total_loss = d_loss_real + d_loss_fake + r1_loss
+        loss_fake = self.bce(fake_output, fake_label)
+        loss = loss_real + loss_fake
+        total_loss = 0.1 * (d_loss_real + d_loss_fake) + 0.1 * r1_loss + loss
         return total_loss
 
-    def calc_G_loss(self, fake_output):
+    def calc_G_loss(self, fake_output, real_label):
         g_loss = F.softplus(-fake_output).mean()
-        return g_loss
+        loss = self.bce(fake_output, real_label)
+        return g_loss * 0.1 + loss
 
     def update(self, loss, opt):
         self.scaler.scale(loss).backward()
         self.scaler.step(opt)
         self.scaler.update()
 
-    def tostep(self,gen,disc):
-        gen.setp()
-        disc.setp()
--- training/training_loop.py
+++ training/training_loop.py
@@ -1,5 +1,5 @@
 import torch
-from torch import nn, optim
+from torch import nn
 from torchvision import utils
 from tqdm import tqdm
 from pathlib import Path
@@ -8,9 +8,9 @@ import logging
 from torch.nn import functional as F
 from training.loss import StyleGAN2Loss
 from utils.dataloader import get_dataloader
-from utils.init import init_generator, init_discriminator, init_model, init_schedulers, init_optimizers, \
+from utils.init import init_generator, init_discriminator, init_schedulers, init_optimizers, \
     device_initializer, seed_initializer, amp_initializer
-from utils.metrics import compute_ssim
+from utils.metrics import compute_ssim, compute_psnr, compute_mse, compute_lpips
 
 LOGGER = logging.getLogger(__name__)
 
@@ -19,7 +19,7 @@ class GANTrainer:
     def __init__(self, config):
 
         self.config = config
-        self.device = device_initializer()
+        self.device = device_initializer(0, True)
         # 初始化配置
         self._init_config()
         seed_initializer()
@@ -27,43 +27,42 @@ class GANTrainer:
         self.train_loader = get_dataloader(config)
         self.gen = init_generator(config, self.device)
         self.disc = init_discriminator(config, self.device)
-        init_model(self.gen, self.disc, config)
         self.opt_gen, self.opt_disc = init_optimizers(config, self.gen, self.disc)
         self.scheduler_gen, self.scheduler_disc = init_schedulers(config, self.opt_gen, self.opt_disc)
         self.criterion = self._get_loss()
         # 初始化固定噪声
         self._init_fixed_noise()
-        # 恢复训练
-        self._resume_checkpoint()
         # 训练状态
         self.current_epoch = 0
         self.avg_ssim = 0
         self.best_ssim = 0
+        # 恢复训练
+        self._resume_checkpoint()
+
 
     def _init_config(self):
         """解析配置文件"""
         # 数据配置
-        data_cfg = self.config.dataset
-        self.img_channels = data_cfg.img_channels
+
+        self.img_channels = self.config.img_channels
 
         # 模型配置
-        model_cfg = self.config.model
-        self.z_dim = model_cfg.z_dim
-        self.w_dim = model_cfg.w_dim
-        self.num_classes = model_cfg.num_classes
+        self.z_dim = self.config.z_dim
+        self.w_dim = self.config.w_dim
+        self.num_classes = self.config.num_classes
 
         # 训练配置
-        train_cfg = self.config.train
-        self.epochs = train_cfg.epochs
-        self.gen_lr = train_cfg.gen_lr
-        self.disc_lr = train_cfg.disc_lr
-        self.betas = tuple(train_cfg.betas)
-        self.save_dir = Path(train_cfg.save_dir)
-        self.save_interval = train_cfg.save_interval
-        self.update_emas = train_cfg.update_emas
-        self.truncation_psi = train_cfg.truncation_psi
-        self.r1_gamma = train_cfg.r1_gamma
-        self.r1_ssim = train_cfg.r1_ssim
+
+        self.epochs = self.config.epochs
+        self.gen_lr = self.config.gen_lr
+        self.disc_lr = self.config.disc_lr
+        self.betas = tuple(self.config.betas)
+        self.save_dir = Path(self.config.save_dir)
+        self.save_interval = self.config.save_interval
+        self.update_emas = self.config.update_emas
+        self.truncation_psi = self.config.truncation_psi
+        self.r1_gamma = self.config.r1_gamma
+        self.r1_ssim = self.config.r1_ssim
         # 创建保存目录
         self.save_dir.mkdir(parents=True, exist_ok=True)
 
@@ -82,10 +81,9 @@ class GANTrainer:
         ).float().to(self.device)
 
     def _resume_checkpoint(self):
-        """恢复训练检查点"""
-        if not self.config.train.get("resume"):
+        if self.config.resume is None:
             return
-        checkpoint = torch.load(self.config.train.resume)
+        checkpoint = torch.load(self.config.resume)
         self.gen.load_state_dict(checkpoint['gen_state_dict'])
         self.disc.load_state_dict(checkpoint['disc_state_dict'])
         self.opt_gen.load_state_dict(checkpoint['gen_optimizer'])
@@ -126,33 +124,43 @@ class GANTrainer:
             inputs = inputs.to(self.device)
             target = target.to(self.device)
             batch_size = inputs.size(0)
+            # The real sample label is 1, and the generated sample label is 0.
+            real_label = torch.full((batch_size, 1), 1, dtype=inputs.dtype).to(device=self.device, non_blocking=True)
+            fake_label = torch.full((batch_size, 1), 0, dtype=inputs.dtype).to(device=self.device, non_blocking=True)
             noise = torch.randn([batch_size, self.z_dim], device=self.device)
             conditional = F.one_hot(target, self.num_classes).to(self.device).float()
             self.disc.zero_grad()
-            with torch.amp.autocast(self.device, enabled=self.device.type != "cpu"):
+            with torch.amp.autocast(self.device.type, enabled=self.device.type != "cpu"):
                 inputs.requires_grad_(True)
                 real_output = self.criterion.run_D(inputs, conditional)
-            with torch.amp.autocast(self.device, enabled=self.device.type != "cpu"):
+            with torch.amp.autocast(self.device.type, enabled=self.device.type != "cpu"):
                 fake = self.criterion.run_G(noise, conditional)
                 fake_output = self.criterion.run_D(fake.detach(), conditional)
-            total_loss = self.criterion.calc_D_loss(inputs, real_output, fake_output)
+            total_loss = self.criterion.calc_D_loss(inputs, real_output, fake_output, real_label, fake_label)
             self.criterion.update(total_loss, self.opt_disc)
             self.gen.zero_grad()
-            with torch.amp.autocast(self.device, enabled=self.device.type != "cpu"):
+            with torch.amp.autocast(self.device.type, enabled=self.device.type != "cpu"):
                 fake_output = self.criterion.run_D(fake, conditional)
-            g_loss = self.criterion.calc_G_loss(fake_output)
+            g_loss = self.criterion.calc_G_loss(fake_output, real_label)
             ssim = compute_ssim(inputs, fake)
-            g_loss_ssim = g_loss + self.r1_ssim * (1 - ssim)
+            mse = compute_mse(inputs, fake)
+            psnr = compute_psnr(mse)
+            lpips = compute_lpips(inputs, fake)
+            g_loss_ssim = g_loss + self.r1_ssim * (1 - ssim) + 0.25 * mse.item() + lpips * 0.5
             self.criterion.update(g_loss_ssim, self.opt_gen)
             progress_bar.set_postfix({
                 'D Loss': total_loss.item(),
                 'G Loss': g_loss_ssim.item(),
                 'ssim': ssim,
+                "mse": mse.item(),
+                "psnr":psnr,
+                "lpips": lpips,
             })
             self.ssims.append(ssim)
 
     def after_training(self, epoch: int):
-        self.criterion.tostep(self.scheduler_gen, self.scheduler_disc)
+        self.scheduler_gen.step()
+        self.scheduler_disc.step()
         avg_ssim = sum(self.ssims) / len(self.ssims)
         if (epoch + 1) % self.save_interval != 0:
             return
